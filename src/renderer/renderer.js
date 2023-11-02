5f8b547c2fb84e79cf2acbaec61e44e9652ffb76
Aviator2276 <91645065+Aviator2276@users.noreply.github.com>
2023-11-02 17:20:38 -0400

    Working on media controls

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,8 +1,13 @@
 let mainVideo  = document.getElementById("mainVideo");
+const play = document.getElementById('play')
 
-function playPauseMainVideo() { 
-    if (mainVideo.paused) 
-      mainVideo.play(); 
-    else 
-      mainVideo.pause(); 
-  } 
+function playPauseMainVideo() {
+  if (mainVideo.paused) {
+    mainVideo.play();
+    play.style.animation = "playDisappear .5s forwards";
+  } else {
+    mainVideo.pause();
+    play.style.animation = "playAppear .5s forwards";
+  }
+}
+//mainVideo.addEventListener("ended", play.style.animation = "playAppear .5s forwards", false);
